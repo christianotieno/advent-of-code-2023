a3916de6c679d7c3d68937e9a5c70eab20ca6fa9
Christian Otieno <c.otieno@merix.com.pl>
2024-12-04 06:36:59 +0300

    chore: remove commented out code

--- adventofcode2023/internal/day1/day1.go
+++ adventofcode2023/internal/day1/day1.go
@@ -40,20 +40,6 @@ func PartTwo(input string) (int, error) {
 			}
 
 			wordDigits := []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"}
-
-			
-			// _wordDigits := map[string]string {
-			// 	"one": "1",
-			// 	"two": "2",
-			// 	"three": "3",
-			// 	"four": "4",
-			// 	"five": "5",
-			// 	"six": "6",
-			// 	"seven": "7",
-			// 	"eight": "8",
-			// 	"nine": "9",
-			// }
-
 			for d, word := range wordDigits {
 				if strings.HasPrefix(line[i:], word) {
 					digits = append(digits, []rune(strconv.Itoa(d+1))...)
@@ -61,11 +47,6 @@ func PartTwo(input string) (int, error) {
 			}
 		}
 
-		// runeSlices := []rune(strconv.Itoa(d+1))
-		// digits = append(digits, runeSlices[0])
-		// digits = append(digits, runeSlices...)
-
-		
 		if len(digits) > 0 {
 			value, _ := strconv.Atoi(string(digits[0]) + string(digits[len(digits)-1]))
 			sum += value
